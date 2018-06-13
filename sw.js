09b5ffc62413528af1d1549e8db2b6bdbf16a1f9
Saingyouth Yem <saingyouthy@zillowgroup.com>
2018-06-12 20:42:56 -0700

    Final work without Aria

---
+++ sw.js
@@ -0,0 +1,50 @@
+const version = 2;
+const CACHE_NAME = `my-site-cache-v${version}`;
+const OLD_CACHE_NAME = `my-site-cache-v${version - 1}`;
+const urlsToCache = [
+  '/',
+  'css/styles.css',
+  'js/main.js',
+  'js/dbhelper.js',
+  'js/restaurant_info.js',
+  'data/restaurants.json',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg',
+  'index.html'
+];
+
+
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.delete(OLD_CACHE_NAME)
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) return response;
+        return fetch(event.request);
+      }
+    )
+  );
+});
